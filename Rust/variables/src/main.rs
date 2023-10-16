0eb76a05e1ea3316766357d219c65c5db57596e8
Hamunki <howard@astucemedia.com>
2023-10-16 09:43:07 +0100

    Rust tutorial files

---
+++ Rust/variables/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    const SECONDS_IN_A_DAY: u32 = 24*60*60;
+    let mut x = 5;
+    println!("x = {x}");
+    x = 6;
+    println!("x = {x}");
+    println!("SECONDS_IN_A_DAY: {SECONDS_IN_A_DAY}");
+    println!("==================================");
+
+    let incremental = 1;
+    println!("incremental: {incremental}");
+    {
+        let incremental = incremental + 2;
+        println!("incremental: {incremental}");
+    }
+    println!("incremental: {incremental}");
+
+
+    println!("==================================");
+
+    let tup = (500, 6.4, true, "Sausage", 'A');
+    println!("3: {}", tup.3);
+
+
+}
